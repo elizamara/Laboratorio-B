ecd9861ffff3bac3d9d46acd20355b40eb4f7343
Elizamara <elizamara.almeida@icomp.ufam.edu.br>
2024-07-15 11:24:20 -0400

    Add servidor

---
+++ Lab8/server.go
@@ -0,0 +1,13 @@
+package main
+
+import "net/http"
+
+func main(){
+// Fornece a pasta onde ta o arqv index.html
+
+	folderSystem := http.FileServer(http.Dir("./src/"))
+
+	http.Handle("/", folderSystem)
+
+	http.ListenAndServe(":8080", nil)
+}
