4b84113343e21eb60d4e123085138eb59258c752
Elizamara <elizamara.almeida@icomp.ufam.edu.br>
2024-07-25 10:25:19 -0400

    Lab9

--- Lab8/server.go
+++ Lab8/server.go
@@ -6,7 +6,6 @@ func main(){
 // Fornece a pasta onde ta o arqv index.html
 
 	folderSystem := http.FileServer(http.Dir("./src/"))
-
 	http.Handle("/", folderSystem)
 
 	http.ListenAndServe(":8080", nil)
